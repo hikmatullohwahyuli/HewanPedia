c5736f77c31c3d8ecfd30da201e2784751e5eab2
hikmatullohwahyuli <hikmatullahwahyuli@gmail.com>
2020-11-23 20:20:33 +0700

    Hewan Pedia

---
+++ app/src/main/java/simple/example/hewanpedia/DataProvider.java
@@ -0,0 +1,93 @@
+package simple.example.hewanpedia;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import simple.example.hewanpedia.model.Anjing;
+import simple.example.hewanpedia.model.Beruang;
+import simple.example.hewanpedia.model.Hewan;
+import simple.example.hewanpedia.model.Kucing;
+
+public class DataProvider {
+    private static List<Hewan> hewans = new ArrayList<>();
+
+    private static List<Beruang> intDataBeruang(Context ctx) {
+        List<Beruang> beruangs = new  ArrayList<>();
+        beruangs.add(new Beruang("BERUANG HITAM", "AMERIKA UTARA",
+                "Beruang hitam amerika adalah omnivora. Makanan mereka lebih banyak terdiri dari tumbuhan, tapi mereka juga makan ikan dan dan serangga. Mereka tidak terancam punah, tapi perburuan liar berpotensi menggerus populasi mereka.",
+                R.drawable.beruang_hitam_amerika));
+        beruangs.add(new Beruang("BERUANG COKLAT","AMERIKA UTARA",
+                "Seperti beruang hitam, beruang coklat juga umum terdapat di kawasan Amerika Utara. Dan seperti beruang hitam amerika pula, beruang coklat pun memiliki warna bulu yang bervariasi. Mulai dari coklat kehitaman, coklat muda, hingga pirang.",
+                R.drawable.beruang_coklat));
+        beruangs.add(new Beruang("BERUANG KUTUP","KUTUP UTARA",
+                "Beruang kutub adalah spesies terbesar dari antara semua beruang. Dengan panjang mencapai 3 meter dan berat sampai 800 kg, beruang kutub layak dinobatkan sebagai predator darat terbesar di dunia.",
+                R.drawable.beruang_kutup));
+        beruangs.add(new Beruang("BERUANG HITAM ASIA","AMERIKA",
+                "Beruang hitam asia mirip dengan beruang hitam amerika, tapi mereka punya beberapa perbedaan. Misalnya, beruang hitam asia punya corak berwarna coklat yang khas di dada mereka, sesuatu yang gak dimiliki beruang hitam amerika.",
+                R.drawable.beruang_hitam_asia));
+        return beruangs;
+    }
+
+    private static List<Anjing> initDataAnjing(Context ctx) {
+        List<Anjing> anjings = new ArrayList<>();
+        anjings.add(new Anjing("Bulldog", "Inggris",
+                "Anjing populer yang dikenal dengan badan yang gemuk berotot, wajah seperti kain kusut, dan hidung yang pesek", R.drawable.dog_bulldog));
+        anjings.add(new Anjing("Husky", "Alaska,Siberia,Finlandia (daerah bersalju) ",
+                "Anjing jenis khusus yang awalnya digunakan untuk menarik kereta salju", R.drawable.dog_husky));
+        anjings.add(new Anjing("Kintamani", "Indonesia",
+                "Ras anjing asli pulau Bali ", R.drawable.dog_kintamani));
+        anjings.add(new Anjing("Samoyed", "Rusia",
+                "Anjing yang berasal dari Siberia yang awalnya biasa digunakan untuk menjaga ternak oleh suku Samoyed", R.drawable.dog_samoyed));
+        anjings.add(new Anjing("Shepherd", "Jerman",
+                "Anjing pintar dan kuat, sekarang banyak digunakan sebagai anjing penjaga, pengawal, dan anjing polisi (K9)", R.drawable.dog_shepherd));
+        anjings.add(new Anjing("Shiba", "Jepang",
+                "Anjing dari daerah Shiba ini gesit dan lincah sehingga awalnya banyak digunakan untuk berburu ", R.drawable.dog_shiba));
+        return anjings;
+    }
+
+    private static List<Kucing> initDataKucing(Context ctx) {
+        List<Kucing> kucings = new ArrayList<>();
+        kucings.add(new Kucing("Angora", "Turki",
+                "Aslinya berasal dari Ankara, Turki dengan ciri khas berbulu panjang dan lembut", R.drawable.cat_angora));
+        kucings.add(new Kucing("Bengal", "Inggris",
+                "Merupakan hasil persilangan antara Kucing Leopard Asia dengan kucing domestik Inggris", R.drawable.cat_bengal));
+        kucings.add(new Kucing("Birmani", "Birma/Myanmar",
+                "Konon awalnya merupakan ras kucing yand biasa menemani para biksu yang menetap pada kuil di Birma", R.drawable.cat_birman));
+        kucings.add(new Kucing("Persia", "Iran",
+                "Kucing berbulu panjang dengan wajah bundar dan hidung pesek. Awalnya berasal dari Iran kemudian diimpor ke Italia", R.drawable.cat_persia));
+        kucings.add(new Kucing("Siam", "Thailand",
+                "Kucing hasil turunan dari Wichian Maat (Kucing Thailand) dengan ciri khas berbadan panjang dan langsing, wajah lonjong, telinga lebar, dan mata kebiruan", R.drawable.cat_siam));
+        kucings.add(new Kucing("Siberia", "Rusia",
+                "Kucing domestik Rusia dengan bulu lebat dan badan besar tapi sangat lincah dan terkenal dengan kemampuan melompatnya ", R.drawable.cat_siberian));
+        return kucings;
+    }
+
+    private static void initAllHewans(Context ctx) {
+        hewans.addAll(initDataKucing(ctx));
+        hewans.addAll(initDataAnjing(ctx));
+        hewans.addAll(intDataBeruang(ctx));
+    }
+
+    public static List<Hewan> getAllHewan(Context ctx) {
+        if (hewans.size() == 0) {
+            initAllHewans(ctx);
+        }
+        return  hewans;
+    }
+
+    public static List<Hewan> getHewansByTipe(Context ctx, String jenis) {
+        List<Hewan> hewansByType = new ArrayList<>();
+        if (hewans.size() == 0) {
+            initAllHewans(ctx);
+        }
+        for (Hewan h : hewans) {
+            if (h.getJenis().equals(jenis)) {
+                hewansByType.add(h);
+            }
+        }
+        return hewansByType;
+    }
+
+}
---
+++ app/src/main/java/simple/example/hewanpedia/model/Beruang.java
@@ -0,0 +1,9 @@
+package simple.example.hewanpedia.model;
+
+public class Beruang extends Hewan {
+
+    public Beruang(String ras, String asal, String deskripsi, int drawableRes) {
+        super("Beruang", ras, asal, deskripsi, drawableRes);
+
+    }
+}
